fedf952988b3708b72d5723ec2a992a6c763a8d0
Johongirr <johongir_rahimov@bk.ru>
2021-01-20 10:03:09 +0100

    completed home page of travel company website

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const collapseBtn2 = document.querySelector('.collapse-2');
 const openMenuBtn = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.mobile-menu');
 const closeMenuBtn = document.querySelector('.mobile-menu__close');
-
 let isSubMenu1Open = false;
 let isSubMenu2Open = false;
 let isMenOpen = false;
+let counter = 0;
+
 
 const toggleSubMenu1 = (e)=>{
+    collapseBtn1.classList.toggle("show");
     if(!isSubMenu1Open){
         subMenu1.style.display = "block";
+        collapseBtn1.style.transform = "rotate(270deg)";
         isSubMenu1Open = true;
     } else {
         subMenu1.style.display = "none";
+        collapseBtn1.style.transform = "rotate(90deg)";
         isSubMenu1Open = false;
     }
 }
@@ -23,26 +27,35 @@ const toggleSubMenu1 = (e)=>{
 const toggleSubMenu2 = (e)=>{
     if(!isSubMenu2Open){
         subMenu2.style.display = "block";
+        collapseBtn2.style.transform = "rotate(270deg)";
         isSubMenu2Open = true;
     } else {
         subMenu2.style.display = "none";
+        collapseBtn2.style.transform = "rotate(90deg)";
         isSubMenu2Open = false;
     }
 }
 
 const toggleMenu = ()=>{
+    
     if(!isMenOpen){
-        menu.style.display = "block";
+        console.log("IT'S WORKING!!!!");
+        
+        menu.style.transform = "translateX(0)";
         isMenOpen = true;
         closeMenuBtn.addEventListener('click', ()=>{
             console.log(213123)
             if(isMenOpen){
-                menu.style.display = "none";
+                menu.style.transform = "translateX(20rem)";
                 isMenOpen = false;
             }
         })
     } 
 }
+
+ 
+
 collapseBtn1.addEventListener('click', toggleSubMenu1);
 collapseBtn2.addEventListener('click', toggleSubMenu2);
 openMenuBtn.addEventListener('click', toggleMenu);
+
